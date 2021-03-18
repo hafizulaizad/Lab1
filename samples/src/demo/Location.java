e3e3657e6373ee8c13d483e474b908c935f9db29
Hafizul Aizad <hafizulm.noor@gmail.com>
2021-03-18 12:21:07 +0800

    Third commit

--- samples/src/demo/Location.java
+++ samples/src/demo/Location.java
@@ -4,14 +4,32 @@ public class Location {
 	
 	// Init private var 
 	private String state = "Kuching";
+	private String country = "Malaysia";
+	private String name = "Hafizul";
 	
 	// Getter
 	public String getState() {
 	return state;
 	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 	// Setter
 	public void setState(String changeState) {
 	this.state = changeState;
+	}
+	
+	public void setCountry(String changeCountry) {
+	this.state = changeCountry;
+	}
+	
+	public void setName(String changeName) {
+		this.state = changeName;
 	}
 }
